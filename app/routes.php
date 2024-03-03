7943c63b2d17298df0aceb1086b5d4ec4812f624
Simon Merrick <s.m3rrick@gmail.com>
2024-03-03 21:52:49 +1300

    Troubleshoot traces between two instances

--- app/routes.php
+++ app/routes.php
@@ -8,10 +8,42 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use OpenTelemetry\API\Trace\TracerProviderInterface;
+use OpenTelemetry\Context\Context;
+use GuzzleHttp\Client;
+
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use OpenTelemetry\API\Trace as API;
+// use OpenTelemetry\Context\Context;
+use OpenTelemetry\SDK\Trace\TracerProvider;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
         // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->get('/foo', function (Request $request, Response $response) {
+        $response->getBody()->write("bar");
+        return $response;
+    });
+
+    $app->get('/ping', function (Request $request, Response $response) {
+        $count = (int)($request->getQueryParams()['count'] ?? '1');
+
+        $client = new Client();
+        $url = getenv("LINK_INTERNAL") . "/foo";
+
+        $response->getBody()->write("pong " . $count);
+
+        if ($count > 0) {
+
+            $res = $client->request('GET', $url, [
+                "count" => $count - 1
+            ]);
+            echo $res->getBody() . " ";
+        }
         return $response;
     });
 
@@ -22,7 +54,7 @@ return function (App $app) {
         $color = getenv("COLOR");
 
         echo "<h1 style=\"color: ". $color . "\">" . $name . "</h1>";
-        echo "<a href=\"" . $link . "\">Go to " . $link .  "</a><br><br>";
+        echo "<a href=\"" . $link . "/foo\">Go to " . $link .  "</a><br><br>";
         echo "Jaeger: <a target=\"_blank\" href=\"http://localhost:16686\">http://localhost:16686</a><br><br>";
 
         $extensions = get_loaded_extensions();
